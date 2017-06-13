ce383408fb7c69d1906f9fc03e7fc5a34ef82590
Altiarika <yeonaltiarika@users.noreply.github.com>
2017-06-13 14:40:25 +0700

    Initial Commit

---
+++ StyleLongation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StyleLongation
+//
+//  Created by Altiarika on 6/13/17.
+//  Copyright © 2017 Altiarika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
